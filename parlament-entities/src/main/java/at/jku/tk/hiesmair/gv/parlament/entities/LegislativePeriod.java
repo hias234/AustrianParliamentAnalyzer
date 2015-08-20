5075fd3530afb6083db835a41676e9dfef5eeb2d
Markus Hiesmair <markus.hiesmair@yahoo.de>
2015-08-20 16:02:31 +0200

    Added more fields to entities

--- parlament-entities/src/main/java/at/jku/tk/hiesmair/gv/parlament/entities/LegislativePeriod.java
+++ parlament-entities/src/main/java/at/jku/tk/hiesmair/gv/parlament/entities/LegislativePeriod.java
@@ -1,8 +1,11 @@
 package at.jku.tk.hiesmair.gv.parlament.entities;
 
+import java.util.List;
+
 public class LegislativePeriod {
 
 	private Integer period;
+	private List<Session> sessions;
 
 	public Integer getPeriod() {
 		return period;
@@ -10,6 +13,14 @@ public class LegislativePeriod {
 
 	public void setPeriod(Integer period) {
 		this.period = period;
+	}
+
+	public List<Session> getSessions() {
+		return sessions;
+	}
+
+	public void setSessions(List<Session> sessions) {
+		this.sessions = sessions;
 	}
 	
 	
--- parlament-entities/src/main/java/at/jku/tk/hiesmair/gv/parlament/entities/Session.java
+++ parlament-entities/src/main/java/at/jku/tk/hiesmair/gv/parlament/entities/Session.java
@@ -3,28 +3,44 @@ package at.jku.tk.hiesmair.gv.parlament.entities;
 import java.util.Date;
 
 public class Session {
-	
+
 	private Integer sessionNr;
 	private Date startDate;
 	private Date endDate;
+
+	public Session() {
+		super();
+	}
+
+	public Session(Integer sessionNr, Date startDate, Date endDate) {
+		super();
+		this.sessionNr = sessionNr;
+		this.startDate = startDate;
+		this.endDate = endDate;
+	}
+
 	public Integer getSessionNr() {
 		return sessionNr;
 	}
+
 	public void setSessionNr(Integer sessionNr) {
 		this.sessionNr = sessionNr;
 	}
+
 	public Date getStartDate() {
 		return startDate;
 	}
+
 	public void setStartDate(Date startDate) {
 		this.startDate = startDate;
 	}
+
 	public Date getEndDate() {
 		return endDate;
 	}
+
 	public void setEndDate(Date endDate) {
 		this.endDate = endDate;
 	}
-	
-	
+
 }
