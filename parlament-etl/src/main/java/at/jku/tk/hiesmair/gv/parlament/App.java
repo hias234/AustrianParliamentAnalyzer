7d41827efaa059d2187c29464340066d19416a68
Markus Hiesmair <markus.hiesmair@yahoo.de>
2015-08-24 19:00:23 +0200

    renamed parlament to parlament-etl

--- parlament-etl/src/main/java/at/jku/tk/hiesmair/gv/parlament/App.java
+++ parlament-etl/src/main/java/at/jku/tk/hiesmair/gv/parlament/App.java
@@ -1,15 +1,7 @@
 package at.jku.tk.hiesmair.gv.parlament;
 
-import java.io.IOException;
-import java.text.ParseException;
 import java.util.Arrays;
 
-import javax.xml.parsers.ParserConfigurationException;
-
-import org.w3c.dom.DOMException;
-import org.xml.sax.SAXException;
-
-import at.jku.tk.hiesmair.gv.parlament.feed.FeedReader;
 import at.jku.tk.hiesmair.gv.parlament.period.LegislativePeriodEtlJob;
 import at.jku.tk.hiesmair.gv.parlament.period.extractor.LegislativePeriodExtractor;
 import at.jku.tk.hiesmair.gv.parlament.period.loader.ConsoleLegislativePeriodLoader;
@@ -21,20 +13,14 @@ import at.jku.tk.hiesmair.gv.parlament.period.transformer.LegislativePeriodTrans
 public class App {
 
 	/**
-	 * Create a {@link FeedReader} download everything and analyse everything
+	 * download everything and analyse everything
 	 * 
 	 * @param args
-	 * @throws InterruptedException
-	 * @throws IOException
-	 * @throws ParseException
-	 * @throws ParserConfigurationException
-	 * @throws SAXException
-	 * @throws DOMException
 	 */
 	public static void main(String[] args) throws Exception {
 		LegislativePeriodEtlJob job = new LegislativePeriodEtlJob(new LegislativePeriodExtractor(),
 				new LegislativePeriodTransformer(), new ConsoleLegislativePeriodLoader());
 		
-		job.start(Arrays.asList(25, 24, 23, 22, 21));
+		job.start(Arrays.asList(25, 24, 23, 22, 21, 20));
 	}
 }
