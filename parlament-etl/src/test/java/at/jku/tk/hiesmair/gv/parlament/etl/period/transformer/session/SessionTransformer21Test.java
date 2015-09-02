f9dbb371a1ced839b08d33cd32be8b3b2fac2d3b
hias234 <hias234@github.com>
2015-09-02 15:25:33 +0200

    added class for sessiontransformer21-test

---
+++ parlament-etl/src/test/java/at/jku/tk/hiesmair/gv/parlament/etl/period/transformer/session/SessionTransformer21Test.java
@@ -0,0 +1,12 @@
+package at.jku.tk.hiesmair.gv.parlament.etl.period.transformer.session;
+
+import org.junit.Test;
+
+public class SessionTransformer21Test {
+
+	@Test
+	public void testGetSession(){
+		
+	}
+	
+}
