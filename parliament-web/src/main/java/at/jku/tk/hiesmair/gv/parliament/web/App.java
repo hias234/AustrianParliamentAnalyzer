d379da2fbff9a546d49af801a2ede0eb71ed42a7
Markus Hiesmair <markus.hiesmair@yahoo.de>
2015-09-18 10:50:15 +0200

    added web-project

---
+++ parliament-web/src/main/java/at/jku/tk/hiesmair/gv/parliament/web/App.java
@@ -0,0 +1,13 @@
+package at.jku.tk.hiesmair.gv.parliament.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+	
+}
