ad5a525bdc2ca674613c2abb11dae8c1cbf81ffd
Markus Hiesmair <markus.hiesmair@yahoo.de>
2015-09-23 17:58:26 +0200

    optimized politician graph

--- parliament-web/src/main/java/at/jku/tk/hiesmair/gv/parliament/web/service/PoliticianAttitudeService.java
+++ parliament-web/src/main/java/at/jku/tk/hiesmair/gv/parliament/web/service/PoliticianAttitudeService.java
@@ -10,6 +10,7 @@ import javax.inject.Inject;
 import org.springframework.stereotype.Service;
 
 import at.jku.tk.hiesmair.gv.parliament.db.repositories.relation.PoliticianAttitudeRelationRepository;
+import at.jku.tk.hiesmair.gv.parliament.entities.mandate.NationalCouncilMember;
 import at.jku.tk.hiesmair.gv.parliament.entities.politician.Politician;
 import at.jku.tk.hiesmair.gv.parliament.entities.relation.PoliticianAttitudeRelationByPeriod;
 import at.jku.tk.hiesmair.gv.parliament.web.dto.graph.D3Graph;
@@ -33,49 +34,51 @@ public class PoliticianAttitudeService {
 	public D3Graph getPoliticianAttitudeGraph(Integer period){
 		List<PoliticianAttitudeRelationByPeriod> clubAttitudes = getPoliticianAttitudes(period);
 		
-		List<D3Node> nodes = getNodes(clubAttitudes);
-		List<D3Link> links = getLinks(clubAttitudes, nodes);
+		List<D3Node> nodes = getNodes(clubAttitudes, period);
+		List<D3Link> links = getLinks(clubAttitudes, nodes, period);
 		
 		return new D3Graph(nodes, links);
 	}
 
-	protected List<D3Link> getLinks(List<PoliticianAttitudeRelationByPeriod> politicianAttitudes, List<D3Node> nodes) {
+	protected List<D3Link> getLinks(List<PoliticianAttitudeRelationByPeriod> politicianAttitudes, List<D3Node> nodes, Integer period) {
 		List<D3Link> links = new ArrayList<D3Link>();
 
-		Integer maxAbsWeight = politicianAttitudes.stream().mapToInt(ca -> Math.abs(ca.getWeight())).max().getAsInt();
-		
 		for (PoliticianAttitudeRelationByPeriod politicianRelation : politicianAttitudes){
 			if (Math.abs(politicianRelation.getWeight()) > 3 && !politicianRelation.getPolitician1().equals(politicianRelation.getPolitician2())) {
-				links.add(getLink(politicianRelation, nodes, maxAbsWeight));
+				links.add(getLink(politicianRelation, nodes, period));
 			}
 		}
 		return links;
 	}
 
-	protected List<D3Node> getNodes(List<PoliticianAttitudeRelationByPeriod> politicianAttitudes) {
+	protected List<D3Node> getNodes(List<PoliticianAttitudeRelationByPeriod> politicianAttitudes, Integer period) {
 		Set<D3Node> nodes = new HashSet<D3Node>();
 		for (PoliticianAttitudeRelationByPeriod politicianAttitude : politicianAttitudes){
-			nodes.add(getNode(politicianAttitude.getPolitician1()));
-			nodes.add(getNode(politicianAttitude.getPolitician2()));
+			nodes.add(getNode(politicianAttitude.getPolitician1(), period));
+			nodes.add(getNode(politicianAttitude.getPolitician2(), period));
 		}
 		return new ArrayList<D3Node>(nodes);
 	}
 	
-	protected D3Node getNode(Politician politician) {
-		return new D3Node(politician.getId(), politician.getSurName());
+	protected D3Node getNode(Politician politician, Integer period) {
+		List<NationalCouncilMember> ncMandates = politician.getNationalCouncilMemberMandates(period);
+		String color = null;
+		if (!ncMandates.isEmpty()){
+			color = ncMandates.get(0).getClub().getColor();
+		}
+		
+		return new D3Node(politician.getId(), politician.getSurName(), color);
 	}
 	
-	protected D3Link getLink(PoliticianAttitudeRelationByPeriod clubRelation, List<D3Node> nodes, Integer maxAbsWeight){
-		Integer sourceIndex = getNodeIndex(clubRelation.getPolitician1(), nodes);
-		Integer targetIndex = getNodeIndex(clubRelation.getPolitician2(), nodes);
-		
-//		Double weight = clubRelation.getWeight() / Double.valueOf(maxAbsWeight);
+	protected D3Link getLink(PoliticianAttitudeRelationByPeriod clubRelation, List<D3Node> nodes, Integer period){
+		Integer sourceIndex = getNodeIndex(clubRelation.getPolitician1(), nodes, period);
+		Integer targetIndex = getNodeIndex(clubRelation.getPolitician2(), nodes, period);
 		Double weight = Double.valueOf(clubRelation.getWeight());
 		
 		return new D3Link(sourceIndex, targetIndex, weight);
 	}
 	
-	protected Integer getNodeIndex(Politician politician, List<D3Node> nodes) {
-		return nodes.indexOf(getNode(politician));
+	protected Integer getNodeIndex(Politician politician, List<D3Node> nodes, Integer period) {
+		return nodes.indexOf(getNode(politician, period));
 	}
 }
