7a413cb184923e19ec81e6131a5798755bfead70
Markus Hiesmair <markus.hiesmair@yahoo.de>
2015-09-27 22:12:00 +0200

    show only clubs of national council

--- parliament-web/src/main/resources/static/js/base.js
+++ parliament-web/src/main/resources/static/js/base.js
@@ -2,7 +2,7 @@ function getClubsFromPolitician(p) {
 	var distinctClubs = [];
 	var distinctClubNames = [];
 	for (var i = 0; i < p.mandates.length; i++){
-		if (p.mandates[i].club != null){
+		if (p.mandates[i].type == 'NationalCouncilMember'){
 			if (distinctClubNames.indexOf(p.mandates[i].club.shortName) == -1){
 				distinctClubs.push(p.mandates[i].club);
 				distinctClubNames.push(p.mandates[i].club.shortName);
